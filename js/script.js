13dd6b7c1218e0d19e26fd60285179dcbe95e3d6
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2020-08-17 18:05:53 -0400

    update file structure

--- js/script.js
+++ js/script.js
@@ -1,51 +1,52 @@
+"use strict";
 var quotes = [
     {
         quote: 'If you want others to be happy, practice compassion. If you want to be happy, practice compassion.',
         source: 'Dalai Lama',
         citation: 'https://www.developgoodhabits.com/mindfulness-quotes/',
-        tag: 'Mindfulness'
+        tag: 'Mindfulness',
     },
     {
         quote: 'When we get too caught up in the busyness of the world, we lose connection with one another -- and ourselves.',
         source: 'Jack Kornfield',
         citation: 'https://www.developgoodhabits.com/mindfulness-quotes/',
-        tag: 'Mindfulness'
+        tag: 'Mindfulness',
     },
     {
         quote: 'Let go of your mind and then be mindful. Close your ears and listen!',
         source: 'Jalauddin Rumi',
         citation: 'https://www.developgoodhabits.com/mindfulness-quotes/',
-        tag: 'Mindfulness'
+        tag: 'Mindfulness',
     },
     {
         quote: 'Look at other people and ask yourself if you are really seeing them or just your thoughts about them.',
         source: 'Jon Kabat-Zinn',
         citation: 'https://www.developgoodhabits.com/mindfulness-quotes/',
-        tag: 'Mindfulness'
+        tag: 'Mindfulness',
     },
     {
         quote: "When I'm hungry, I eat what I love. When I'm bored, I do something I love. When I'm lonely, I connect with someone I love. When I feel sad, I remember that I am loved.",
         source: 'Michelle May',
         citation: 'https://www.developgoodhabits.com/mindfulness-quotes/',
-        tag: 'Mindfulness'
+        tag: 'Mindfulness',
     },
     {
         quote: 'The things that matter most in our lives are not fantastic or grand. They are moments when we touch one another.',
         source: 'Jack Kornfield',
         citation: 'https://www.developgoodhabits.com/mindfulness-quotes/',
-        tag: 'Mindfulness'
+        tag: 'Mindfulness',
     },
     {
         quote: "The first step toward your success is the one 'you' take, not someone else.",
         source: 'Ricky DeFazio',
         year: 2018,
-        tag: 'Motivation'
+        tag: 'Motivation',
     },
     {
         quote: "The usefulness of having a goal isn't it's eventual attainment, but rather the actions it manifests.",
         source: 'Ricky DeFazio',
         year: 2018,
-        tag: 'Motivation'
+        tag: 'Motivation',
     },
 ];
 var randomIndex = function (array) {
